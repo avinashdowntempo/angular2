f5ee08d8f950e78b3a74135084910b4e8130c438
avinashdowntempo <avinashj4394@gmail.com>
2017-05-21 17:06:23 +0530

    some clean up in map data

--- src/app/dashboard/account-list.component.ts
+++ src/app/dashboard/account-list.component.ts
@@ -7,54 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AccountListComponent {
   accounts = ACCOUNTS;
-  data = [83, 75, 67, 50, 90, 35];
-   labels = ['DS', 'LD', 'EC', 'M3', 'DS Lab', 'EC Lab'];
-   columnTypes = [{
-                'type': 'string',
-                'value': 'subjectName'
-              },
-                {
-                  'type': 'number',
-                  'value': 'Marks'
-                }];
-  chartType = 'Pie';
-  options = {
-                'width': 550,
-                'height': 300,
-                'pieHole': 0.4,
-                'bars': 'vertical',
-                'chartArea': { 'left': 50, 'bottom': 50, 'right': 100, 'top': 50 }
-              };
-  /*pieChartOptions =  [{
-  chartType: 'PieChart',
-  dataTable: [
-    ['Task', 'Hours per Day'],
-    ['Work',     11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
-  ],
-  options: {'title': 'Tasks', 'pieHole': '0.4'},
-},
-{
-  chartType: 'PieChart',
-  dataTable: [
-    ['Task', 'Hours per Day'],
-    ['Global Equity',     11],
-    ['Canadian Equity',      2],
-    ['Canadian Balanced',  2]
-  ],
-  options: {
-    'width': '300',
-    'height': '300',
-    'title': 'By Asset Class',
-    'pieHole': '0.4',
-    'chartArea': { 'left': '5' }
-    },
-}];*/
-
-
   onSelect(mod) {
     if ( mod.show === false){
       mod.show = true;
@@ -66,16 +18,16 @@ export class AccountListComponent {
 export class Account {
   name: string;
   id: number;
-  subaccounts: [{show: boolean; accname: String; acctype: String; mrkvalue: number; }];
+  subaccounts: [{data: number[]; labels: String[]; show: boolean; accname: String; acctype: String; mrkvalue: number; }];
 }
 const ACCOUNTS: Account[] = [
   { id: 189986, name: 'Dr. Robert Grewal',
-  subaccounts: [{show: false, accname: '11A28865 - Self-Directed LIRA', acctype: 'Univeris Nominee', mrkvalue: 857369.92 },
-  {show: false, accname: '11A28851 - SDRSP', acctype: 'Univeris Nominee', mrkvalue: 247868.61 },
-  {show: false, accname: 'Open or Non-Registered', acctype: 'Univeris Client Name', mrkvalue: 129267.55 } ]},
+  subaccounts: [{data: [41.93, 35.09, 22.99], labels: ['Global Equity', 'Canadian Equity', 'Canadian Balanced'], show: false, accname: '11A28865 - Self-Directed LIRA', acctype: 'Univeris Nominee', mrkvalue: 857369.92 },
+  {data: [42.75, 21.6, 35.65], labels: ['Global Equity', 'Canadian Equity', 'Canadian Balanced'], show: false, accname: '11A28851 - SDRSP', acctype: 'Univeris Nominee', mrkvalue: 247868.61 },
+  {data: [100], labels: ['Canadian Balanced'], show: false, accname: 'Open or Non-Registered', acctype: 'Univeris Client Name', mrkvalue: 129267.55 } ]},
   { id: 190549, name: 'Dr. Robert H Grewal & Mrs. Judy Grewal',
-  subaccounts: [{show: false, accname: '11A28865 - Self-Directed LIRA', acctype: 'Univeris Nominee', mrkvalue: 857369.92 },
-  {show: false, accname: '11A28851 - SDRSP', acctype: 'Univeris Nominee', mrkvalue: 247868.61 },
-  {show: false, accname: 'Open or Non-Registered', acctype: 'Univeris Client Name', mrkvalue: 129267.55 } ]}
+  subaccounts: [{data: [63.02, 26.67, 10.31], labels: ['Global Equity', 'Canadian Equity', 'U.S.Equity'], show: false, accname: '11A28865 - Self-Directed LIRA', acctype: 'Univeris Nominee', mrkvalue: 857369.92 },
+  {data: [42.75, 21.6, 35.65], labels: ['Global Equity', 'Canadian Equity', 'Canadian Balanced'], show: false, accname: '11A28851 - SDRSP', acctype: 'Univeris Nominee', mrkvalue: 247868.61 },
+  {data: [100,50], labels: ['Canadian Balanced','U.S.Equity'], show: false, accname: 'Open or Non-Registered', acctype: 'Univeris Client Name', mrkvalue: 129267.55 } ]}
 ];
 
--- src/app/dashboard/map-show.component.ts
+++ src/app/dashboard/map-show.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'map-show',
   templateUrl: './map-show.component.html',
-  inputs: ["show","data","labels","columnTypes","chartType","options"]
+  inputs: ["show","data","labels"]
 })
 export class MapShowComponent {
+    columnTypes = [{
+                'type': 'string',
+                'value': 'subjectName'
+              },
+                {
+                  'type': 'number',
+                  'value': 'Marks'
+                }];
+  chartType = 'Pie';
+  options = {
+                'width': 550,
+                'height': 300,
+                'pieHole': 0.4,
+                'bars': 'vertical',
+                'chartArea': { 'left': 50, 'bottom': 50, 'right': 100, 'top': 50 }
+              };
 }
 
